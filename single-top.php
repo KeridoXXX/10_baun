c2954c7e13d3e84a516e548c83411e64a5dbac87
sylle <sylledejlig@gmail.com>
2022-05-31 14:30:18 +0200

    skolecommit1

--- single-top.php
+++ single-top.php
@@ -22,13 +22,27 @@ get_header(); ?>
 <main id="main">
 
 	<article class="single-top">
-		<img class="billede1" src="" alt="">
-		<img class="billede2" src="" alt="">
-		<img class="billede3" src="" alt="">
-		<h2></h2>
-		<p class="top-beskrivelse"></p>
-		<p class="top-pris"></p>
-		
+		<div class="col-left">
+				<img class="billede1" src="" alt="">
+				<img class="billede2" src="" alt="">
+				<img class="billede3" src="" alt="">
+		</div>
+		<div class="col-right">
+			<h2></h2>
+			<h3></h3>
+			<p class="top-beskrivelse"></p>
+			<p class="top-pris"></p>
+			<div class="sizes">
+				<button class="small">S</button>
+				<button class="medium">M</button>
+				<button class="large">L</button>
+			</div>
+			<p>All bodies are perfect - <a href="https://lehmannen.dk/kea/10_baun/size-guide/">see our sizeguide</a></p>
+			
+			<p> 20% Nylon 80% Elastene <br> Designed in Denmark by Alberte Muff</p>
+			<p>The model is 183cm and wears a size M</p>
+			<img src="http://lehmannen.dk/kea/10_baun/wp-content/uploads/2022/05/1280px-Chris_Hemsworth_Signature.png" alt="">
+		</div>
 	</article>
 
 </main>
@@ -50,11 +64,13 @@ get_header(); ?>
 	}
 
 	function showTopSingular() {
-		// console.log(topSingular.title.rendered)
-		// console.log(topSingular.billede1.guid)
+		console.log(topSingular)
 		document.querySelector(".billede1").src = topSingular.billede1.guid;
 		document.querySelector(".billede2").src = topSingular.billede2.guid;
 		document.querySelector(".billede3").src = topSingular.billede3.guid;
+		document.querySelector("h2").textContent = topSingular.title.rendered;
+		document.querySelector(".top-beskrivelse").textContent = topSingular.title.rendered;
+		document.querySelector(".top-pris").textContent = topSingular.pris;
 	}
 
 
